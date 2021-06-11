c0f84750059d89ebd0d99dd4d138d28a060e54b9
Svyatoslav <s.chaikun@bars-open.ru>
2021-06-11 18:31:59 +0300

    Change interface to abstractclass

---
+++ MLNet/Services/Telegram/Abstractions/Command.cs
@@ -0,0 +1,15 @@
+﻿using Telegram.Bot;
+using Telegram.Bot.Types;
+
+namespace MLNet.Services.Telegram.Abstractions
+{
+    public abstract class Command
+    {
+        public abstract string Name { get; }
+        public abstract void Execute(Message message, TelegramBotClient client);
+        public bool Contains(string command)
+        {
+            return command.Contains(this.Name);
+        }
+    }
+}
