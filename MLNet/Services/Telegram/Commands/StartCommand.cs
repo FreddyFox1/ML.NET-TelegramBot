b503737f9c034eadb2ffd01ddd1238d8e4c8b073
Svyatoslav <s.chaikun@bars-open.ru>
2021-06-11 18:32:36 +0300

    Start Command

---
+++ MLNet/Services/Telegram/Commands/StartCommand.cs
@@ -0,0 +1,20 @@
+﻿using MLNet.Services.Telegram.Abstractions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Telegram.Bot;
+using Telegram.Bot.Types;
+
+namespace MLNet.Services.Telegram.Commands
+{
+    public class StartCommand : Command
+    {
+        public override string Name => "/start";
+
+        public async override void Execute(Message message, TelegramBotClient client)
+        {
+            await client.SendTextMessageAsync(message.Chat.Id, "Отправь мне фотографию кота или собаки и я угадаю, кто на ней изображен :)");
+        }
+    }
+}
